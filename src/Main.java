21806f27a7739f293ee5a540fa19727901052819
GVabal <gvabal@gmail.com>
2020-01-23 19:45:11 +0200

    V2 init

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
-        primaryStage.setTitle("Commitments and Deposits Puller");
+        primaryStage.setTitle("Commitments and Deposits Puller V2");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
